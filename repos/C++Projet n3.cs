364ae9242a4994e056ed2f6368ff6ceb167d6726
OliviaTshibumbu <oliviatshibumbu2006@gmail.com>
2025-05-15 15:16:28 +0200

    New commit

---
+++ repos/C++Projet n3.cs
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+    int entier1;
+    cout << "saisir le troisieme entier" << endl;
+    cin >> entier1;
+    if (entier1 < 0)
+    {
+        cout << "votre nombre est negatif!" << endl;
+    }
+    else if (entier1 > 0)
+    {
+        cout << "votre nombre est positif" << endl;
+    }
+    else
+    {
+        cout << "zero" << endl;
+    }
+    return 0;
+}
